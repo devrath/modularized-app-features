8e5a6f930e186f2ad5ca5c330c795609260e20d1
Devrath <devrath.dev595@gmail.com>
2021-06-14 12:00:32 +0530

    v

---
+++ Application/features/login/src/main/java/com/example/login/LoginActivity.kt
@@ -0,0 +1,9 @@
+package com.example.login
+
+import com.example.base.BaseActivity
+
+class LoginActivity : BaseActivity() {
+
+
+
+}
---
+++ Application/features/login/src/main/java/com/example/login/LoginActivityNavigation.kt
@@ -0,0 +1,7 @@
+package com.example.login
+
+import android.app.Activity
+
+interface LoginActivityNavigation {
+    fun finishLogin(activity: Activity)
+}
