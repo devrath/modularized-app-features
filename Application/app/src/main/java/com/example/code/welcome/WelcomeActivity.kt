d514e1ee2d04257e1439a7bd4911f4a5755704f9
Devrath <devrath.dev595@gmail.com>
2021-06-15 09:26:49 +0530

    v

--- Application/app/src/main/java/com/example/code/welcome/WelcomeActivity.kt
+++ Application/app/src/main/java/com/example/code/welcome/WelcomeActivity.kt
@@ -1,20 +1,20 @@
 package com.example.code.welcome
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.code.databinding.ActivitySplashBinding
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
-import java.lang.Exception
 
 @AndroidEntryPoint
 class WelcomeActivity : AppCompatActivity() {
 
     companion object {
         const val FEATURE_LOGIN = "com.example.login.LoginActivity"
+        const val ERROR_IN_FEATURE_LAUNCH = "Error in launching feature "
     }
 
     private lateinit var binding: ActivitySplashBinding
@@ -38,8 +38,8 @@ class WelcomeActivity : AppCompatActivity() {
                 Class.forName(FEATURE_LOGIN)?.let { feature ->
                     startActivity(Intent(this@WelcomeActivity, feature))
                 }
-            }catch (exception : Exception){
-                Timber.e("Error in launching feature: $exception")
+            }catch (exception: Exception){
+                Timber.e(ERROR_IN_FEATURE_LAUNCH.plus(exception))
             }
         }
     }
