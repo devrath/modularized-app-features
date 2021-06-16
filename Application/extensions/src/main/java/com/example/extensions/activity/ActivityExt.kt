f98f61f4a2788bc752c26c3c6df642ae281ce219
Devrath <devrath.dev595@gmail.com>
2021-06-16 22:46:19 +0530

    vvvv

---
+++ Application/extensions/src/main/java/com/example/extensions/activity/ActivityExt.kt
@@ -0,0 +1,14 @@
+package com.example.extensions.activity
+
+import android.app.Activity
+import android.util.DisplayMetrics
+
+
+
+
+
+fun Activity.isInPortrait(): Boolean {
+    val displayMetrics = DisplayMetrics()
+    windowManager.defaultDisplay.getMetrics(displayMetrics)
+    return displayMetrics.heightPixels > displayMetrics.widthPixels
+}
---
+++ Application/extensions/src/main/java/com/example/extensions/fragment/FragmentManagerExt.kt
@@ -0,0 +1,16 @@
+package com.example.extensions.fragment
+
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+
+inline fun FragmentManager.inTransactionCommit(func: FragmentTransaction.() -> Unit) {
+    val fragmentTransaction = beginTransaction()
+    fragmentTransaction.func()
+    fragmentTransaction.commit()
+}
+
+inline fun FragmentManager.inTransactionCommitAllowingStateLoss(func: FragmentTransaction.() -> Unit) {
+    val fragmentTransaction = beginTransaction()
+    fragmentTransaction.func()
+    fragmentTransaction.commitAllowingStateLoss()
+}
